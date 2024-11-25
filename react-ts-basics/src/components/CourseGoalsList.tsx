6a9bd996c6c485fe5bfc21b18bc00c76d2a3d8dc
Einav Haimovich <einav@haimol.net>
2024-11-25 22:16:30 +0200

    end of first course project

--- react-ts-basics/src/components/CourseGoalsList.tsx
+++ react-ts-basics/src/components/CourseGoalsList.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { CourseGoalType } from "../types/CourseGoals";
 import CourseGoal from "./CourseGoals";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
@@ -8,8 +9,26 @@ type CourseGoalListProps = {
 };
 
 function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet, Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length > 3) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You have a lot of goals. Consider deleting some of them.
+      </InfoBox>
+    );
+  }
   return (
-    <div>
+    <>
+    {warningBox}
       <ul>
         {!!goals &&
           goals.map((goal) => (
@@ -23,7 +42,7 @@ function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
             </li>
           ))}
       </ul>
-    </div>
+    </>
   );
 }
 
